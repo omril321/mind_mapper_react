e0b5af1be9b0a381aa7d9ed90c6f7f08daee37ae
Omri Lavi <omril321@gmail.com>
2017-10-28 18:37:35 +0300

    updated tests for loading page

--- test/components/LoadingPage/LoadingPage.test.tsx
+++ test/components/LoadingPage/LoadingPage.test.tsx
@@ -1,10 +1,13 @@
-import { shallow, render, mount } from 'enzyme';
+import {shallow, render, mount} from 'enzyme';
 import LoadingPage from "../../../src/components/LoadingPage/LoadingPage";
 import * as React from "react";
 
-it('should contain a proper text', () => {
-    const wrapper = render(
-        <LoadingPage isLoading={true}/>
-    );
+it('should contain loading text when loading', () => {
+    const wrapper = render(<LoadingPage isLoading={true}/>);
     expect(wrapper.text()).toBe("Loading!");
+});
+
+it('should not contain anything when not loading', () => {
+    const wrapper = render(<LoadingPage isLoading={false}/>);
+    expect(wrapper.empty()).toBeTruthy()
 });
