21a622319c3acd0356aee224421a93bfc5195df8
Omri Lavi <omril321@gmail.com>
2017-10-29 09:05:49 +0200

    added tests for history visit

---
+++ test/dto/HistoryVisit.test.ts
@@ -0,0 +1,21 @@
+import HistoryVisit from "../../src/dto/HistoryVisit";
+
+describe('HistoryVisit', () => {
+    it('should return the history item title using the proper getter', () => {
+        const visit = new HistoryVisit({
+            id: 100,
+            title: "this is a test!"
+        });
+
+        expect(visit.getTitle()).toBe("this is a test!");
+    });
+
+    it('should return the history item title using the proper getter', () => {
+        const visit = new HistoryVisit({
+            id: 200,
+            url: "some url"
+        });
+
+        expect(visit.getVisitUrl()).toBe("some url");
+    });
+});
