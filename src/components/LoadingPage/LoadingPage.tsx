2f0e1fa9722ee8df41640af655f53b3666d24468
Omri Lavi <omril321@gmail.com>
2019-01-27 22:06:55 +0200

    updated dependencies, and removed some that are unnecessary right now

--- src/components/LoadingPage/LoadingPage.tsx
+++ src/components/LoadingPage/LoadingPage.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import {CSSTransitionGroup} from "react-transition-group";
 import "./LoadingPage.scss";
 
 interface ILoadingPageProps {
@@ -13,18 +12,14 @@ export default class LoadingPage extends React.Component<ILoadingPageProps, {}> 
             (
                 <div key="loading-page" className="loading-page">
                     <p className="loading-page_text">Loading!</p>
-                </div>);
+                </div>
+            );
         const elementToAdd = isLoading ? loadingPage : null;
 
         return (
             <div id="loading-page">
-            <CSSTransitionGroup
-                transitionName={"loading-page_wrapper"}
-                transitionEnterTimeout={1000}
-                transitionLeaveTimeout={1000}
-            >
                 {elementToAdd}
-            </CSSTransitionGroup>
-        </div>);
+            </div>
+        );
     }
 }
